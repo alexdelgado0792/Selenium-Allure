9dcd3d5a20968f198606d8bc89c8e716cbcd969d
AlexDelgado0792 <arekkusu0792@gmail.com>
2019-04-09 15:44:26 -0500

    base project

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AutomationFramework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AutomationFramework")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9fe67ec1-4a5b-40f1-a3a5-83d076eed021")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using Allure.Commons;
+using NUnit.Allure.Attributes;
+using NUnit.Allure.Core;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AutomationFramework
+{
+
+    [TestFixture]
+    [AllureNUnit]
+    //[AllureDisplayIgnored]
+    public class UnitTest1
+    {
+        [Test(Description = "XXX")]
+        [AllureTag("Regression")]
+        [AllureSeverity(SeverityLevel.critical)]
+        //[AllureIssue("ISSUE-1")]
+        //[AllureTms("TMS-12")]
+        [AllureOwner("User")]
+        [AllureSuite("PassedSuite")]
+        [AllureSubSuite("NoAssert")]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver
+            {
+                Url = "https://www.webpage.com/"
+            };
+            driver.Close();
+        }
+    }
+}
